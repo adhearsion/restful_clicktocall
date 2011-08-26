a9ec7f76286af7c70187ed5cdb164cbc82c9d14b
Ben Langfeld <ben@langfeld.me>
2011-08-26 10:23:08 +0100

    Rename some RPC methods to restful

--- restful_clicktocall.rb
+++ restful_clicktocall.rb
@@ -1,6 +1,6 @@
 methods_for :rpc do
   # Helps the GUI determine whether the call is still active.
-  def call_with_destination(destination)
+  def restful_call_with_destination(destination)
     ahn_log.click_to_call "Finding call with destination #{destination.inspect} in #{Adhearsion.active_calls.size} active calls"
     status = find_call_by_destination(destination) ? "alive" : "dead"
     {:result => status}
@@ -8,7 +8,7 @@ methods_for :rpc do
 
   # Traverses Adhearsion's data structure for active calls and returns
   # the proper channel name that Asterisk needs to hangup a call.
-  def hangup_channel_with_destination(destination)
+  def restful_hangup_channel_with_destination(destination)
     if found_call = find_call_by_destination(destination)
       Adhearsion::VoIP::Asterisk.manager_interface.hangup found_call.variables[:channel]
       {:channel => found_call.variables[:channel]}
